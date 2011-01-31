f32383955f9b7a37786104f65b3243178720dade
Joseph Liu <froseph@gmail.com>
2011-01-30 17:49:01 -0800

    Change event description to be a text area

--- VRForms.py
+++ VRForms.py
@@ -3,8 +3,7 @@ from flaskext.wtf import Form, TextField, DateField, IntegerField, validators, B
 class addevent(Form):
   name = TextField('Event Name', [validators.Length(min=1, max=63)])
   date = DateField('Date', [validators.Required('Need to provide a date')])
-  price = IntegerField('Default Price', [validators.optional(), validators.NumberRange(min=None, max=None)])
-  description = TextField('Description', [validators.Length(min=1, max=16384)])
+  description = TextAreaField('Description', [validators.Length(min=1, max=16384)])
 
 class addcustomer(Form):
   first_name = TextField('First Name', [validators.Length(min=1, max=63)])
